38f59e8310b75568bfad1afabd15c6fc8231f39d
ClaFiore <claborghini@gmail.com>
2020-08-25 12:31:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+
+  let form = document.querySelector("form#create-task-form")
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let newul = document.createElement("ul")
+    let newli = document.createElement("li")
+    
+
+    newli.innerText = document.querySelector("input#new-task-description").value;
+    //event.target[1].value
+    
+    console.log("ok")
+
+    newul.append(newli)
+    let list = document.querySelector('ul#tasks')
+    list.append(newul)
+    // document.getElementById("tasks").innerHTML += <code>preventDefault()</code>;
+    
+    // let div = document.querySelector("#tasks")
+    // div.appendChild(content)
+
+
+},false);
+
+
+})
+  
+
