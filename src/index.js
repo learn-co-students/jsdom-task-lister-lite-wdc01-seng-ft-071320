15631035062670dd2ceddea4f62c856cec504df5
slee713 <steven.lee1393@gmail.com>
2020-08-25 14:45:32 -0400

    added button to delete todo item

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let count = 1
   
   document.getElementById("create-task-form").addEventListener("submit", function(event) {
     event.preventDefault();
     let userInput = document.getElementById('new-task-description'); 
     let li = document.createElement('li');
-    li.innerText = `${userInput.value}`;
+    li.id = `${count}`
+    li.innerHTML = `${userInput.value} <span> <button type="button" data-num="${count}">X</button></span>`
     document.getElementById("tasks").append(li);
+    event.target.reset();
+    count++;
+
   });
+
+  let list = document.querySelector("#tasks")
+  list.addEventListener("click", function() {
+    let num = event.target.dataset.num;
+    let li = document.getElementById(`${num}`).remove()
+  })
+  
   
   
   
