857a051274829fa31304b137201fff0c3f85742f
slee713 <steven.lee1393@gmail.com>
2020-08-25 10:07:16 -0400

    found basic solution

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   
   document.getElementById("create-task-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    let userInput = document.getElementById('new-task-description').value; 
+    let userInput = document.getElementById('new-task-description'); 
     let li = document.createElement('li');
-    li.innerText = `${userInput}`;
-    document.getElementById("tasks").append(li)
+    li.innerText = `${userInput.value}`;
+    document.getElementById("tasks").append(li);
   });
   
   
